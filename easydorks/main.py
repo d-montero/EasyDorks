eb95feb090cde4470f8643956db87d0ff8a72939
d-montero <d.montero.2021@alumnos.urjc.es>
2023-10-31 15:20:37 +0100

    limpieza y modularizacion de codigo

--- easydorks/main.py
+++ easydorks/main.py
@@ -1,95 +1,19 @@
-import urllib.parse
 import webbrowser
 import customtkinter as ctk
+import dork_functions as df
 from PIL import Image
 
 # https://github.com/d-montero/EasyDorks
 result = "https://www.google.com"
 
-
-# dork functions
-
-dorksFiletype = [
-    "filetype:pdf",
-    "filetype:doc",
-    "filetype:txt",
-    "filetype:jpg",
-    "filetype:xls",
-    "filetype:ppt",
-    "filetype:html",
-    "filetype:xml",
-]
-
-dorksInurl = [
-    "inurl:"
-]
-
-dorksInsite = [
-    "intext:"
-
-]
-
-dorksExtension = [
-    "extension:json",
-    "extension:xml",
-    # Agrega más dorks personalizados según sea necesario
-]
-
-
-def fileTypeDork(file_type):
-    results = []
-    for dork in dorksFiletype:
-        if dork.split(":")[1] == file_type:
-            query = f"{dork}"
-            encoded_query = urllib.parse.quote(query)
-            search_url = f"https://www.google.com/search?q={encoded_query}"
-            results.append(search_url)
-    return results
-
-
-def searchInurlDork(keyword):
-    results = []
-    for dork in dorksInurl:
-        query = f"{dork}{keyword}"
-        encoded_query = urllib.parse.quote(query)
-        search_url = f"https://www.google.com/search?q={encoded_query}"  # Cadena segura para url escapando caracteres especiales que no deberian estar
-        results.append(search_url)
-    return results
-
-
-def searchInsiteDork(keyword):
-    results = []
-    for dork in dorksInsite:
-        query = f"{dork}{keyword}"
-        encoded_query = urllib.parse.quote(query)
-        search_url = f"https://www.google.com/search?q={encoded_query}"
-        results.append(search_url)
-    return results
-
-
-def ExtensionDork(keyword):
-    results = []
-    for dork in dorksFiletype:
-        if dork.split(":")[1] == keyword:
-            query = f"{dork} {keyword}"
-            encoded_query = urllib.parse.quote(query)
-            search_url = f"https://www.google.com/search?q={encoded_query}"
-            results.append(search_url)
-    return results
-
-
-def nada(arg):
-    return [""]
-
-
 # do function
 
 
 str_to_func = {
-    "Elige opción": nada,
-    "archivos tipo:": fileTypeDork,
-    "que la url contenga:": searchInurlDork,
-    "que la página contenga": searchInsiteDork
+    "Elige opción": df.nada,
+    "archivos tipo:": df.fileTypeDork,
+    "que la url contenga:": df.searchInurlDork,
+    "que la página contenga": df.searchInsiteDork
 }
 
 
